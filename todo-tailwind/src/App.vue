43842cfcadd693c04c1fa76f77b945855bc66b18
Andres Ballesteros <amballesterosr@ufpso.edu.co>
2023-11-29 16:34:27 -0500

    TaskList

---
+++ todo-tailwind/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+import TaskComponent from '@/components/TaskComponent.vue'
+import {ref} from 'vue';
+
+const tareas = ref(['Ninguna','Dormir','Descansar'])
+
+function deleteTask(index){
+  tareas.value.splice(index, 1)
+}
+
+</script>
+
+<template>
+ <div class="mt-12 px-4 py-2 w-96 bg-gray-600 shadow-md rounded mx-auto">
+  
+  <h1 class="text-center text-2xl font-semibold">
+    Lista de tareas
+  </h1>
+
+  <div class="flex mt-2">
+
+    <input type="text" 
+    class="outline-none rounded-r-none w-4/5 h-8 px-2 py-1 border border-green-600 rounded">
+
+    <span class="transition-colors duration-300 hover:cursor-pointer hover:bg-green-700 rounded-l-none w-1/5 h-8 text-xl font-bold text-center text-white bg-green-600 rounded">
+      +
+    </span>
+
+  </div>
+
+  <TaskComponent
+  v-for="(tarea, index) in tareas"
+  :key="index"
+  :tarea="tarea"
+  :index="index"
+  @borrarHijo= "deleteTask"
+  />
+
+ </div>
+</template>
---
+++ todo-tailwind/src/components/TaskComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+const props = defineProps(
+    {
+    tarea: {
+        type: String,
+        default: 'Tarea por defecto'
+    },
+    index: {
+        type: Number
+    }
+    }
+)
+
+const emit = defineEmits([ 'borrarHijo' ])
+
+function borrar (index){
+    //console.log('Borrar hijo', index)
+    emit('borrarHijo', index)
+}
+
+</script>
+
+<template>
+
+    <div class="transition-colors duration-300 hover:bg-gray-300 mt-1 px-4 py-1 flex justify-between bg-gray-200 rounded">
+
+    {{props.tarea}}
+
+<span
+@click="borrar(props.index)"
+ class="px-2 transition-colors duration-300 hover:cursor-pointe hover:bg-red-900 hover:cursor-pointer  text-xl text-center text-white bg-red-600 rounded">
+  X
+</span>
+
+</div>
+
+
+</template>
